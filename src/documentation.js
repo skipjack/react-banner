4c7412cc82336d698f396fc34c020a2a576ca7a9
Greg Venech <greg.venech@gmail.com>
2016-12-29 09:50:15 -0500

    Renaming the documentation entry point and adding a Site component

---
+++ src/documentation.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Site from './site'
+
+render((
+    <Site />
+), document.querySelector('#root'))
---
+++ src/site.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Banner from './banner'
+import Sections from './sections'
+import './site-style'
+
+const block = 'site'
+
+export default props => {
+    return (
+        <div className={ block }>
+            <Banner
+                searching
+                sections={ Sections }
+                url={ window.location.pathname } />
+
+            <main className={ `${block}__content` }>
+                <div className={ `${block}__container` }>
+                    Some content
+                </div>
+            </main>
+
+            <footer className={ `${block}__footer` }>
+                <div className={ `${block}__container` }>
+                    
+                </div>
+            </footer>
+        </div>
+    )
+}
