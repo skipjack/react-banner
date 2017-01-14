47a79a6e469e890c40a12db0442ba5c91e155bc0
Greg Venech <greg.venech@gmail.com>
2017-01-14 14:41:23 -0500

    Another `/docs` update

--- webpack.docs.js
+++ webpack.docs.js
@@ -9,7 +9,7 @@ module.exports = {
 
     output: {
         path: Path.resolve(__dirname, './docs'),
-        publicPath: '/',
+        publicPath: Production ? '' : '/',
         filename: 'root.bundle.js'
     },
 
