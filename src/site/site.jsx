0a42d4a3d6b3d75f717640978957ce20a95ed909
Greg Venech <greg.venech@gmail.com>
2017-10-16 23:36:49 -0400

    chore(docs): add search functionality to the docs site
    This provides a nice way to demonstrate the search features although
    more documentation is still needed.

--- src/site/site.jsx
+++ src/site/site.jsx
@@ -1,9 +1,14 @@
+// Import External Deps
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import Algolia from 'algoliasearch/lite'
 import Sidebar from 'react-sidebar'
+
+// Import Local Deps
 import Banner from '../banner/banner'
 import Logo from '../logo/logo'
 import SPALink from '../links/spa-link'
+import SearchResults from '../search-results/search-results'
 import SiteLinks from './site-links'
 
 // Load Styling
@@ -13,14 +18,21 @@ import './site-style'
 // Specify BEM block name
 const block = 'site'
 
+// Set up algolia
+const client = Algolia('BH4D9OD16A', '9a5a75e0182379c6190f2e511b20d3a7')
 
+// Create and export the component
 export default class Site extends React.Component {
     state = {
-        sidebar: false
+        loading: true,
+        content: '',
+        sidebar: false,
+        search: '',
+        search_hits: null
     }
 
     render() {
-        let { match } = this.props
+        let { search_hits } = this.state
 
         return (
             <Sidebar
@@ -40,7 +52,13 @@ export default class Site extends React.Component {
                     link={ SPALink }
                     links={ SiteLinks }
                     url={ this.props.location.pathname }
-                    onMenuClick={ this._openSidebar } />
+                    searchResults={ search_hits ? (
+                        <SearchResults
+                            query={ this.state.search }
+                            results={ search_hits } />
+                    ) : null }
+                    onMenuClick={ this._openSidebar }
+                    onSearchTyping={ this._search } />
 
                 <main className={ `${block}__content` }>
                     <div className={ `${block}__container` }>
@@ -100,5 +118,29 @@ export default class Site extends React.Component {
 
     _openSidebar = () => {
         this._toggleSidebar(true)
+    }
+
+    /**
+     * Search the given input via our Algolia index
+     * 
+     * @param {string} input - The current search input
+     */
+    _search = (input = '') => {
+        if ( input.length > 0 ) {
+            client.search([{
+                indexName: 'react-banner',
+                query: input,
+                params: {
+                    hitsPerPage: 5
+                }
+            }]).then(data => this.setState({
+                search: input,
+                search_hits: data.results[0].hits
+            }))
+
+        } else this.setState({
+            search: '',
+            search_hits: null
+        })
     }
 }
